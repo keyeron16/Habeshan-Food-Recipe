c936bc35ed37e9cd098ae51b17b16b14304e0872
Hamerti <90256940+Hamerti1@users.noreply.github.com>
2022-06-14 11:23:36 -0700

    Update home.dart

--- habesha_recipe_app/lib/screen/home.dart
+++ habesha_recipe_app/lib/screen/home.dart
@@ -8,13 +8,24 @@ class HomePage extends StatelessWidget {
       child: GridView.builder(
           shrinkWrap: false,
           itemCount: Data.recipes.length,
-         
-  
-   
-       );
-      },
-    }),
-   );
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2),
+          itemBuilder: (BuildContext context, int index) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: InkWell(
+                
+           onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => DetailsPage(
+                                recipe: Data.recipes[index],
+             ))
+           );
+         },
+      }),
+    );
 
     return Scaffold(
 
