aeb5537b224da3a2432fa59f7163a1a54f19d8db
Hamerti <90256940+Hamerti1@users.noreply.github.com>
2022-06-14 11:18:42 -0700

    Update home.dart

--- habesha_recipe_app/lib/screen/home.dart
+++ habesha_recipe_app/lib/screen/home.dart
@@ -4,7 +4,11 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-  
+  var body = Container(
+      child: GridView.builder(
+          shrinkWrap: false,
+          itemCount: Data.recipes.length,
+         
   
    
           }),
