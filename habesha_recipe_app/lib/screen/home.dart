81f6262f5204b019e59e08f5d4e9669fa7c2cdb5
SosinaHizkiel <90421818+SosinaHizkiel@users.noreply.github.com>
2022-06-14 11:34:53 -0700

    Update home.dart

--- habesha_recipe_app/lib/screen/home.dart
+++ habesha_recipe_app/lib/screen/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import '../screens/details.dart';
+import '../utils/data.dart';
+import 'package:flutter/material.dart';
 
 
 class HomePage extends StatelessWidget {
