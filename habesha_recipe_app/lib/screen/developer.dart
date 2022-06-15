2f3c192d2c90cd8793a46fafb0ddb2a01e8f8b1a
abigaeltiezazu <102549247+abigaeltiezazu@users.noreply.github.com>
2022-06-15 10:40:47 -0700

    Update developer.dart

--- habesha_recipe_app/lib/screen/developer.dart
+++ habesha_recipe_app/lib/screen/developer.dart
@@ -14,13 +14,35 @@ class DeveloperScreen extends StatelessWidget {
         padding: const EdgeInsets.all(20),
         children: const <Widget>[
            CircleAvatar(
-        
+             maxRadius: 30,
+            backgroundColor: Color.fromARGB(255, 112, 110, 110),
+            child: Icon(Icons.person,
+                color: Color.fromARGB(255, 124, 70, 0), size: 50),
           ),
           Center(
-            
+               child: Text(
+              'Keyeron Sisay',
+              style: TextStyle(
+                fontSize: 27,
+                fontWeight: FontWeight.bold,
           ),
           Text(
-           
+             "Information  System 3rd Year Student @wku.",
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+          
+           CircleAvatar(
+       
+          ),
+          Center(
+         
+              ),
+            ),
+          ),
+          Text(
+          
             ),
           ),
           
