0b0a8666747c8747713a0632ef6134fa82d06f5e
abigaeltiezazu <102549247+abigaeltiezazu@users.noreply.github.com>
2022-06-15 10:43:16 -0700

    Update developer.dart

--- habesha_recipe_app/lib/screen/developer.dart
+++ habesha_recipe_app/lib/screen/developer.dart
@@ -46,6 +46,27 @@ class DeveloperScreen extends StatelessWidget {
             ),
           ),
           const Divider(),
+          CircleAvatar(
+            maxRadius: 30,
+            backgroundColor: Color.fromARGB(255, 112, 110, 110),
+            child: Icon(Icons.person,
+                color: Color.fromARGB(255, 124, 70, 0), size: 50),
+          ),
+          Center(
+            child: Text(
+              'Simret Markos',
+              style: TextStyle(
+                fontSize: 27,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Text(
+            "Information  System 3rd Year Student @wku.",
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
           
           ]
   
