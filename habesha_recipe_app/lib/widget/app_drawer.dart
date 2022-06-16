f70f87fd8da6992552f046a38edec6dc6a9e36b6
Simret79 <90473861+Simret79@users.noreply.github.com>
2022-06-16 15:34:16 +0800

    Update app_drawer.dart

---
+++ habesha_recipe_app/lib/widget/app_drawer.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/screens/developer.dart';
+
+import '../screens/about_app.dart';
+
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
