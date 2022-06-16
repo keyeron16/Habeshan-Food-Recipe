6290acbfca8f3c4a824b1eeebd68388b4f1d585e
Hamerti <90256940+Hamerti1@users.noreply.github.com>
2022-06-16 12:25:31 -0700

    Update about_app.dart

--- habesha_recipe_app/lib/screen/about_app.dart
+++ habesha_recipe_app/lib/screen/about_app.dart
@@ -12,12 +12,12 @@ class AboutScreen extends StatelessWidget {
             child: Text(
               '',
               style: TextStyle(
-                fontSize: 27,
-                fontWeight: FontWeight.bold,
+              fontSize: 27,
+              fontWeight: FontWeight.bold,
               ),
             ),
           ),
-       Text(
+            Text(
             "version  0.1.",
             style: TextStyle(
             fontSize: 16,
@@ -26,52 +26,52 @@ class AboutScreen extends StatelessWidget {
          Divider(),
           Center(
            child: Text(
-              ' ',
-              style: TextStyle(
-              fontSize: 27,
-              fontWeight: FontWeight.bold,
-              ),
-            ),
+           ' ',
+           style: TextStyle(
+           fontSize: 27,
+           fontWeight: FontWeight.bold,
           ),
+        ),
+      ),
          Text(
             "Year released:  2022 GC/2014 E.C",
             style: TextStyle(
             fontSize: 16,
             ),
           ),
-          Divider(),
-           Center(
-            child: Text(
-              '',
-              style: TextStyle(
-                fontSize: 27,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          Text(
+    Divider(),
+         Center(
+           child: Text(
+           '',
+           style: TextStyle(
+           fontSize: 27,
+           fontWeight: FontWeight.bold,
+         ),
+       ),
+         Text(
             "Android  vs iOS.",
             style: TextStyle(
             fontSize: 16,
           ),
         ), 
        Divider(),
-          Center(
-            child: Text(
-              '',
-              style: TextStyle(
-              fontSize: 27,
-              fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          Text(
-            "This application is developed by wolkite university 3rd year information system student in 2022 in titled in habeshan food recipe this application contain detail description of habeshan food.",
-            style: TextStyle(
-            fontSize: 16,
-            ),
-          ),
-        ]
+        Center(
+          child: Text(
+          '',
+          style: TextStyle(
+          fontSize: 27,
+          fontWeight: FontWeight.bold,
+        ),
       ),
+    ),
+       Text(
+          "This application is developed by wolkite university 3rd year information system student in 2022 in titled in habeshan food recipe this application contain detail description of habeshan food.",
+          style: TextStyle(
+          fontSize: 16,
+        ),
+       ),
+      ]
+     ),
     );
+   }
   }
-}
