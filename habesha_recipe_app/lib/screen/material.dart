7abba7628896c278cff1591c440f78628d4eca6f
Simret79 <90473861+Simret79@users.noreply.github.com>
2022-06-17 03:09:27 +0800

    Update material.dart

--- habesha_recipe_app/lib/screen/material.dart
+++ habesha_recipe_app/lib/screen/material.dart
@@ -10,8 +10,16 @@ class DeveloperScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Material selection"),
       ),
-  
-  
-  
-   }
-  }
+       body: ListView(
+         padding: const EdgeInsets.all(20),
+         children: const <Widget>[
+           CircleAvatar(
+            maxRadius: 30,
+            backgroundColor: Color.fromARGB(255, 112, 110, 110),
+            child: Icon(Icons.person,
+            color: Color.fromARGB(255, 124, 70, 0), size: 50),
+            )
+          ]
+        )
+      }
+    }
